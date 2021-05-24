23e3e3ec435b3a09f2d27585b52758644df166ef
Evgeniy Shestakov <Gennadith.bear@gmail.com>
2021-05-24 17:21:18 +0500

    homework-6

--- app/src/main/java/com/shestakov/notesapp/NoteFragment.java
+++ app/src/main/java/com/shestakov/notesapp/NoteFragment.java
@@ -7,41 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NoteFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+
 public class NoteFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    static final String CURRENT_NOTE = "currentNote";
+    private Note note;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NoteFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NoteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NoteFragment newInstance(String param1, String param2) {
+    public static NoteFragment newInstance(Note note) {
         NoteFragment fragment = new NoteFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putParcelable(CURRENT_NOTE, note);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +30,20 @@ public class NoteFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            note = getArguments().getParcelable(CURRENT_NOTE);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+        TextView titleText = view.findViewById(R.id.note_title);
+        TextView noteContentText = view.findViewById(R.id.note_content);
+        TextView dateOfCreationText = view.findViewById(R.id.note_date_of_creation);
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy", Locale.getDefault());
+        dateOfCreationText.setText(String.format("%s", formatter.format(note.getCreationDate().getTime())));
+        titleText.setText(note.getTitle());
+        noteContentText.setText(note.getContent());
+        return view;
     }
 }
